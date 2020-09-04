48438a818fca8c7fb89753f3e169a4614bccce8b
Peter Polidoro <peterpolidoro@gmail.com>
2020-09-04 16:08:55 -0400

    Update to work with ArduinoJson 6

--- src/HplcController/HplcController.cpp
+++ src/HplcController/HplcController.cpp
@@ -103,8 +103,8 @@ void HplcController::stop()
 // floating-point number (float, double)
 // bool
 // const char *
-// ArduinoJson::JsonArray *
-// ArduinoJson::JsonObject *
+// ArduinoJson::JsonArray
+// ArduinoJson::JsonObject
 // const ConstantString *
 //
 // For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
